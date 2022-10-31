097fb9d4577b4b2202172a937e58fe9816a53df2
leivandresen <leiv.andresen@axpo.com>
2022-10-31 10:10:04 +0100

    fixed linting issues.

--- tools/correct_time_offset.py
+++ tools/correct_time_offset.py
@@ -1,12 +1,14 @@
 import glob
 import os
 from datetime import *
-from dateutil.relativedelta import *
 
-if __name__ == '__main__':
+from dateutil.relativedelta import relativedelta
+
+if __name__ == "__main__":
     # This script changes the filenames since the raspberry pi had the wrong system time
     files_stick = "/Volumes/SONAR_STICK/recordings_stroppel_weekend/*.mp4"
-    files_local = "/Users/leivandresen/Documents/PROJECTS/SONAR_FISH/Field_test_Stroppel_20to24_10_22/weekend_backup/*.mp4"
+    files_local = "/Users/leivandresen/Documents/PROJECTS/SONAR_FISH/Field_test_Stroppel_20to24_10_22/" \
+                  "weekend_backup/*.mp4"
     files_test = "/Users/leivandresen/Documents/Hydro_code/AXH-SonarFish/old_recording_handler_tests/*.mp4"
 
     offset = relativedelta(hours=4, minutes=48, seconds=46)
@@ -21,9 +23,11 @@ if __name__ == '__main__':
         datetime_rpi = datetime.strptime(name[:-4], "%y-%m-%d_start_%H-%M-%S")
         datetime_real = datetime_rpi + offset
 
-        new_filename = os.path.split(file)[0] + "/" + datetime_real.strftime("%y-%m-%d_start_%H-%M-%S") + ".mp4"
+        new_filename = (
+            os.path.split(file)[0]
+            + "/"
+            + datetime_real.strftime("%y-%m-%d_start_%H-%M-%S")
+            + ".mp4"
+        )
 
         os.rename(file, new_filename)
-
-
-
--- tools/stitch_files_together.py
+++ tools/stitch_files_together.py
@@ -1,14 +1,11 @@
 import glob
-import os
-from datetime import *
-
 import numpy as np
-from dateutil.relativedelta import *
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This script changes the filenames since the raspberry pi had the wrong system time
     files_stick = "/Volumes/SONAR_STICK/recordings_stroppel_weekend/*.mp4"
-    files_local = "/Users/leivandresen/Documents/PROJECTS/SONAR_FISH/Field_test_Stroppel_20to24_10_22/weekend_backup/*.mp4"
+    files_local = "/Users/leivandresen/Documents/PROJECTS/SONAR_FISH/Field_test_Stroppel_20to24_10_22/" \
+                  "weekend_backup/*.mp4"
     files_test = "/Users/leivandresen/Documents/Hydro_code/AXH-SonarFish/old_recording_handler_tests/*.mp4"
 
     filenames = glob.glob(files_local)
@@ -21,11 +18,18 @@ if __name__ == '__main__':
 
     "[0:0][1:0][2:0][3:0][4:0][5:0]concat=n=6:v=1:a=0[v]"
     input_files = "".join([(" -i " + file) for file in interest])
-    input_params = "'" + "".join([f"[{file_no}:0]" for file_no in np.arange(0, len(interest))]) +\
-                   f"concat=n={len(interest)}:v=1:a=0[v]" + "'"
-    command = "ffmpeg " + input_files + " -filter_complex " + input_params +\
-              " -map '[v]' -c:v libx264 -preset medium -crf 40 22-10-20_start_17_51_end_21_51_compressed.mp4"
+    input_params = (
+        "'"
+        + "".join([f"[{file_no}:0]" for file_no in np.arange(0, len(interest))])
+        + f"concat=n={len(interest)}:v=1:a=0[v]"
+        + "'"
+    )
+    command = (
+        "ffmpeg "
+        + input_files
+        + " -filter_complex "
+        + input_params
+        + " -map '[v]' -c:v libx264 -preset medium -crf 40 22-10-20_start_17_51_end_21_51_compressed.mp4"
+    )
 
     print(command)
-
-
