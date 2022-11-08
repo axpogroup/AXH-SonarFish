547bc5ba5811ff64c7e7b0866205d8aeb2c6ddb8
leivandresen <leiv.andresen@axpo.com>
2022-11-08 16:30:05 +0100

    fix

--- upload_to_blob.py
+++ upload_to_blob.py
@@ -1,11 +1,14 @@
-import os, uuid
 import glob
+import os
 import subprocess
-# from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import uuid
 
-recording_files_mac = "/Users/leivandresen/Documents/PROJECTS/SONAR_FISH/Field_test_Stroppel_20to24_10_22/" \
-                      "weekend_backup/"
+from azure.storage.blob import BlobServiceClient
+
+recording_files_mac = (
+    "/Users/leivandresen/Documents/PROJECTS/SONAR_FISH/Field_test_Stroppel_20to24_10_22/"
+    "weekend_backup/"
+)
 recording_files_rpi = "/media/fish-pi/sonar-disk1/Stroppel_ongoing/"
 folder_for_snippets = "./recording_snippets"
 os.makedirs(name=folder_for_snippets, exist_ok=True)
@@ -14,13 +17,25 @@ os.makedirs(name=folder_for_snippets, exist_ok=True)
 recordings = glob.glob(recording_files_mac + "*.mp4")
 recordings.sort()
 
-snippet_path = os.path.join(folder_for_snippets, os.path.splittext(os.path.split(recordings[-2])[-1])[0]) + \
-               "_snippet.mp4"
-snippet_cmd = f"ffmpeg -i {recordings[-2]} -c:v libx264 -preset medium -crf 46 {snippet_path}"
+snippet_path = (
+    os.path.join(
+        folder_for_snippets, os.path.splittext(os.path.split(recordings[-2])[-1])[0]
+    )
+    + "_snippet.mp4"
+)
+snippet_cmd = (
+    f"ffmpeg -i {recordings[-2]} -c:v libx264 -preset medium -crf 46 {snippet_path}"
+)
 success = False
 try:
-    output = subprocess.run(snippet_cmd, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                            text=True)
+    output = subprocess.run(
+        snippet_cmd,
+        check=True,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
     print(output.stdout)
     print("Snippet created: ", snippet_path)
     success = True
@@ -35,7 +50,7 @@ except subprocess.CalledProcessError as e:
 if success:
     try:
         print("Preparing to upload snippet...")
-        connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+        connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
         # Create the BlobServiceClient object
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
@@ -49,13 +64,15 @@ if success:
         upload_file_path = os.path.join(local_path, local_file_name)
 
         # Write text to the file
-        file = open(file=upload_file_path, mode='w')
+        file = open(file=upload_file_path, mode="w")
         file.write("Hello, World!")
         file.close()
 
         # Create a blob client using the local file name as the name for the blob
         container_name = "test"
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+        blob_client = blob_service_client.get_blob_client(
+            container=container_name, blob=local_file_name
+        )
 
         print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
 
@@ -64,5 +81,5 @@ if success:
             blob_client.upload_blob(data)
 
     except Exception as ex:
-        print('Exception:')
+        print("Exception:")
         print(ex)
