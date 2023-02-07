823956908c29b86e5324b0f795b2e5d9705ff9d3
leivandresen <leiv.andresen@axpo.com>
2023-02-07 10:58:28 +0100

    Added trackbars for quick prototyping and removed computationally intensive calculations. Now runs at nearly 100 fps and skips every other frame.

--- algorithm/run_algorithm.py
+++ algorithm/run_algorithm.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     detector = FishDetector(settings_dict)
 
     while input_output_handler.get_new_frame():
-        if float(input_output_handler.frame_no)/2 % 1 != 0:
+        if float(input_output_handler.frame_no) / 2 % 1 != 0:
             continue
         detector.process_frame(input_output_handler.current_raw_frame)
         input_output_handler.handle_output(detector)
