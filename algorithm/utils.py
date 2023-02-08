7aa66b5026f465ebba3b2fafea622e0e76d3a549
leivandresen <leiv.andresen@axpo.com>
2023-02-08 16:54:45 +0100

    Started refactoring: took the frames out of the detector internal state and made functions for runtiming. Adapted the visualization to a point but it still relies on the detector instance for the object history and other data. Some visualization functions are a mess.

--- algorithm/utils.py
+++ algorithm/utils.py
@@ -2,9 +2,7 @@ import cv2 as cv
 
 
 def get_elapsed_ms(start_tick_count):
-    return int(
-        (cv.getTickCount() - start_tick_count) / cv.getTickFrequency() * 1000
-    )
+    return int((cv.getTickCount() - start_tick_count) / cv.getTickFrequency() * 1000)
 
 
 def resize_img(img, scale_percent):
