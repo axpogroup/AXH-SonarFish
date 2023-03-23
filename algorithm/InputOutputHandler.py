05a8bbc86e2abe08ce27626a354cf9b7e77fe64d
leivandresen <leiv.andresen@axpo.com>
2023-03-23 18:39:28 +0100

    Refactored InputOutputHandler.py and some other little modifications.

--- algorithm/InputOutputHandler.py
+++ algorithm/InputOutputHandler.py
@@ -31,12 +31,15 @@ class InputOutputHandler:
                 self.output_dir_name = os.path.join(
                     self.settings_dict["output_directory"],
                     dt.datetime.now(dt.timezone.utc).isoformat(timespec="minutes")
-                    + "_" + self.settings_dict["tag"],
+                    + "_"
+                    + self.settings_dict["tag"],
                 )
                 self.output_dir_name = self.output_dir_name.replace(":", "-")
                 os.makedirs(name=self.output_dir_name, exist_ok=True)
 
-            self.output_csv_name = os.path.join(self.output_dir_name, (self.input_filename[:-4] + ".csv"))
+            self.output_csv_name = os.path.join(
+                self.output_dir_name, (self.input_filename[:-4] + ".csv")
+            )
 
         self.playback_paused = False
         self.usr_input = None
@@ -257,7 +260,8 @@ class InputOutputHandler:
         self.output_dir_name = os.path.join(
             self.settings_dict["output_directory"],
             dt.datetime.now(dt.timezone.utc).isoformat(timespec="minutes")
-            + "_" + self.settings_dict["tag"],
+            + "_"
+            + self.settings_dict["tag"],
         )
         self.output_dir_name = self.output_dir_name.replace(":", "-")
         os.makedirs(name=self.output_dir_name, exist_ok=True)
@@ -291,7 +295,7 @@ class InputOutputHandler:
                 "algorithm/InputOutputHandler.py",
                 "algorithm/DetectedObject.py",
                 "algorithm/visualization_functions.py",
-                "algorithm/utils.py"
+                "algorithm/utils.py",
             ]
             with zipfile.ZipFile(
                 os.path.join(self.output_dir_name, "code.zip"), mode="w"
