aa3ed66dd0dafdd11e7b7c4d70e61f6c0176dd17
christoforoskarvelis <christoforos.karvelis@axpo.com>
2024-02-06 13:54:34 +0100

    more refactoring

---
+++ algorithm/preprocess_raw_videos.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import cv2
+
+
+def down_sample_frame_rate_of_video(input_file: Path, output_file: Path, fps_out: int):
+    vidcap = cv2.VideoCapture(input_file.__str__())
+    assert vidcap.isOpened()
+    width = vidcap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fps_in = vidcap.get(cv2.CAP_PROP_FPS)
+    index_in = -1
+    index_out = -1
+    fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+    video_writer = cv2.VideoWriter(
+        output_file.__str__(),
+        fourcc=fourcc,
+        fps=fps_in,
+        frameSize=(int(width), int(height)),
+    )
+    fail_rate = 0
+    while True:
+        success = vidcap.grab()
+        if success:
+            index_in += 1
+            fail_rate = 0
+            out_due = int(index_in / fps_in * fps_out)
+            if out_due > index_out:
+                success, frame = vidcap.retrieve()
+                if success:
+                    index_out += 1
+                    video_writer.write(frame)
+                    print(f"Frame {index_in} -> {index_out} (due {out_due})")
+        fail_rate += 1
+        if fail_rate > 30:
+            break
+    # Release everything if job is finished
+    vidcap.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    filenames = Path("data/raw/").glob("*.mp4")
+    for file_path in filenames:
+        output_file_path = Path("data/intermediate") / file_path.name
+        down_sample_frame_rate_of_video(
+            input_file=file_path, output_file=output_file_path, fps_out=30
+        )
