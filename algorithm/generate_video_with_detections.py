acb891f4ac1b32a8ba5b0d9f581796fa7656ecc0
npelzmann <41925178+npelzmann@users.noreply.github.com>
2024-08-16 13:49:40 +0200

    Merge pull request #11 from axpogroup/feature/MDS-619/get_previous_video
    MDS-619 find previous video

--- algorithm/generate_video_with_detections.py
+++ algorithm/generate_video_with_detections.py
@@ -56,8 +56,9 @@ def main_draw_annotations(settings_dict: dict):
             )
             pbar.update(1)
 
-    if settings_dict.get("compress_output_video", False):
+    if settings_dict.get("record_output_video", False) and settings_dict.get("compress_output_video", False):
         input_output_handler.compress_output_video()
+        input_output_handler.delete_temp_output_dir()
 
     return None
 
