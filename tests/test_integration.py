aa4eca5040b7b2969b88b4c488ebc674541e5f37
Nicolas Pelzmann <nicolas.pelzmann@gmail.com>
2024-08-16 14:19:09 +0200

    adjust settings yaml for integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -98,7 +98,7 @@ class TestIntegration:
         assert len(labels_csv) > 0
         assert list(labels_csv.columns)[:6] == relevant_csv_columns
 
-        with open("../analysis/demo/demo_settings.yaml") as f:
+        with open("../settings/demo_settings.yaml") as f:
             detection_settings = yaml.load(f, Loader=yaml.SafeLoader)
         detection_settings["mask_directory"] = "../analysis/demo/masks"
         detection_settings["display_output_video"] = False
