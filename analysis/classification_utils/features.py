6281cfc084596bce84df8c1f8eabc8a4624f0f29
Nicolas Pelzmann <nicolas.pelzmann@gmail.com>
2024-08-21 18:02:17 +0200

    add v_x and v_y to features

--- analysis/classification_utils/features.py
+++ analysis/classification_utils/features.py
@@ -81,9 +81,10 @@ def load_csv_with_tiles(path: Path) -> pd.DataFrame:
     return csv_with_tiles_df
 
 
-def _read_mask(mask_path: Union[str, Path]) -> np.ndarray:
-    mask = cv.imread(Path(mask_path).as_posix(), cv.IMREAD_GRAYSCALE)
-    assert mask is not None, f"Could not read mask from {mask_path}"
+def _read_mask(mask_path: Optional[Union[str, Path]] = None) -> np.ndarray:
+    mask = cv.imread(Path(mask_path).as_posix(), cv.IMREAD_GRAYSCALE) if mask_path else np.zeros((1080, 1920))
+    if mask is None:
+        raise ValueError(f"Could not read mask from {mask_path}")
     mask = mask[49:1001, 92:1831]  # Drop the border
     return cv.resize(mask, (480, 270)) > 0
 
@@ -92,7 +93,7 @@ class FeatureGenerator(object):
     def __init__(
         self,
         rake_mask_path: Union[str, Path],
-        flow_area_mask_path: Union[str, Path],
+        flow_area_mask_path: Optional[Union[str, Path]] = None,
         non_flow_area_mask_path: Optional[Union[str, Path]] = None,
         gt_fish_id_yaml: Optional[Union[Path, str]] = None,
         measurements_csv_dir: Union[str, Path] = "",
--- analysis/classification_utils/features_utils.py
+++ analysis/classification_utils/features_utils.py
@@ -13,6 +13,8 @@ def calculate_features(measurements_df: pd.DataFrame, masks: dict[str, np.ndarra
         measurements_df["image_tile"]
     )
     feature_df = measurements_df.groupby("id").apply(lambda x: trace_window_metrics(x, masks))
+    measurements_df["v_x"] = measurements_df[["id", "x"]].groupby("id").diff()
+    measurements_df["v_y"] = measurements_df[["id", "y"]].groupby("id").diff()
     return measurements_df.join(feature_df, on="id", how="left")
 
 
@@ -49,19 +51,19 @@ def trace_window_metrics(detection: pd.DataFrame, masks: dict[str, np.array]) ->
     time_ratio_near_rake, dist_near_rake = calculate_rake_path_ratio(detection, masks["rake_mask"])
     return pd.Series(
         {
-            "v_xr_avg": np.mean(np.diff(detection["x"])),
-            "v_yr_avg": np.mean(detection["v_yr"]),
-            "v_xr_median": np.median(detection["v_xr"]),
-            "v_yr_median": np.median(detection["v_yr"]),
-            "v_smoothed_avg": np.mean(calculate_velocity(detection, smoothing_window=25)),
-            "v_smoothed_std": np.std(calculate_velocity(detection, smoothing_window=25)),
-            "v_10th_percentile": np.percentile(calculate_velocity(detection, smoothing_window=25), 10),
-            "v_30th_percentile": np.percentile(calculate_velocity(detection, smoothing_window=25), 30),
-            "v_50th_percentile": np.percentile(calculate_velocity(detection, smoothing_window=25), 50),
-            "v_70th_percentile": np.percentile(calculate_velocity(detection, smoothing_window=25), 70),
-            "v_90th_percentile": np.percentile(calculate_velocity(detection, smoothing_window=25), 90),
-            "v_95th_percentile": np.percentile(calculate_velocity(detection, smoothing_window=25), 95),
-            "v_99th_percentile": np.percentile(calculate_velocity(detection, smoothing_window=25), 99),
+            "x_displacement": detection["x"].iloc[-1] - detection["x"].iloc[0],
+            "y_displacement": detection["y"].iloc[-1] - detection["y"].iloc[0],
+            "v_x_smoothed_avg": np.mean(calculate_velocity(detection, smoothing_window=20)[0]),
+            "v_y_smoothed_avg": np.mean(calculate_velocity(detection, smoothing_window=20)[1]),
+            "v_smoothed_avg": np.mean(absolute_velocity(detection, smoothing_window=20)),
+            "v_smoothed_std": np.std(absolute_velocity(detection, smoothing_window=20)),
+            "v_10th_percentile": np.percentile(absolute_velocity(detection, smoothing_window=20), 10),
+            "v_30th_percentile": np.percentile(absolute_velocity(detection, smoothing_window=20), 30),
+            "v_50th_percentile": np.percentile(absolute_velocity(detection, smoothing_window=20), 50),
+            "v_70th_percentile": np.percentile(absolute_velocity(detection, smoothing_window=20), 70),
+            "v_90th_percentile": np.percentile(absolute_velocity(detection, smoothing_window=20), 90),
+            "v_95th_percentile": np.percentile(absolute_velocity(detection, smoothing_window=20), 95),
+            "v_99th_percentile": np.percentile(absolute_velocity(detection, smoothing_window=20), 99),
             "v_parallel_river_avg": np.nanmean(detection["v_parallel_river"]),
             "v_orthogonal_river_avg": np.nanmean(detection["v_orthogonal_river"]),
             "v_orthogonal_abs_sum": np.sum(np.abs(detection["v_orthogonal_river"])),
@@ -112,15 +114,23 @@ def calculate_average_bbox_size(group: pd.DataFrame) -> float:
     return np.mean(group["w"] * group["h"])
 
 
-def calculate_velocity(detection: pd.DataFrame, smoothing_window: int = 5) -> np.array:
+def calculate_velocity(
+    detection: pd.DataFrame,
+    smoothing_window: int = 5,
+) -> tuple[np.ndarray, np.ndarray]:
     v_x = np.diff(detection["x"])
     v_y = np.diff(detection["y"])
     try:
         smoothed_v_x = savgol_filter(v_x, window_length=smoothing_window, polyorder=2)
         smoothed_v_y = savgol_filter(v_y, window_length=smoothing_window, polyorder=2)
-        return np.sqrt(smoothed_v_x**2 + smoothed_v_y**2)
+        return smoothed_v_x, smoothed_v_y
     except ValueError:
-        return np.nan
+        return (np.nan, np.nan)
+
+
+def absolute_velocity(detection: pd.DataFrame, smoothing_window: int = 5) -> np.array:
+    smoothed_v_x, smoothed_v_y = calculate_velocity(detection, smoothing_window)
+    return np.sqrt(smoothed_v_x**2 + smoothed_v_y**2)
 
 
 def velocity_relative_to_river_velocity(
