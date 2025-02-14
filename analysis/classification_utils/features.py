791fd753714c560998e4fa1b678062ede4398fde
npelzmann <nicolas.pelzmann@axpo.com>
2025-02-14 10:01:28 +0100

    pre-commit hooks

--- analysis/classification_utils/features.py
+++ analysis/classification_utils/features.py
@@ -316,7 +316,7 @@ class FeatureGenerator(object):
                     save_df.to_csv(cache_path, index=False)
                 except OSError as e:
                     print(f"Error writing to file {cache_path}: {e}. Not saving features to csv.")
-            
+
             df_list.append(labels_df)
 
         return df_list
@@ -459,13 +459,13 @@ class TrackPlotter(object):
                 plotter_df = measurements_dfs[idx].copy(deep=True)
                 if plotter_df.empty:
                     continue
-                
+
                 plotter_df["id"] = plotter_df.video_id.apply(str) + "-" + plotter_df.id.apply(str)
                 self.measurements_dfs.append(plotter_df)
-                
+
                 if csv_paths:
                     self.csv_paths.append(csv_paths[idx])
-                
+
                 if gt_dfs:
                     gt_df = gt_dfs[idx].copy(deep=True)
                     gt_df["id"] = gt_df.video_id.apply(str) + "-" + gt_df.id.apply(str)
